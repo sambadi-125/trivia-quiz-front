e7bb0f86d8ac716bba0f6242887c80f26b067b95
Magomed Abubakarov <magomed.abubakarov@gmail.com>
2026-07-19 01:27:18 +0200

    quiz-logic chore: update quiz logic, clean up, styling

--- src/app/trivia-quiz/trivia-quiz.ts
+++ src/app/trivia-quiz/trivia-quiz.ts
@@ -8,12 +8,12 @@ import {
 } from '@angular/forms';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { TriviaQuizService } from '../services/trivia-quiz-service';
 import { QuizQuestion } from '../models/quiz-question';
 import { PlayerAnswer } from '../models/player-answer';
 import { PlayerAnswerValidationResponse } from '../models/player-answer-validation-response';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-trivia-quiz',
@@ -22,9 +22,9 @@ import { PlayerAnswerValidationResponse } from '../models/player-answer-validati
     CommonModule,
     ReactiveFormsModule,
     MatCardModule,
-    MatRadioModule,
     MatButtonModule,
     NgOptimizedImage,
+    MatButtonToggleModule,
   ],
   templateUrl: './trivia-quiz.html',
   styleUrl: './trivia-quiz.css',
@@ -34,7 +34,7 @@ export class TriviaQuiz implements OnInit {
   playerResponses: PlayerAnswer[] = [];
   playerResponseValidationResults = signal<PlayerAnswerValidationResponse[]>([]);
   quizForm: FormGroup;
-  score: number = 0;
+  score = signal(0);
   showResults = signal(false);
 
   constructor(
@@ -49,7 +49,10 @@ export class TriviaQuiz implements OnInit {
       next: (data) => {
         this.allQuestions = data;
         this.allQuestions.forEach((quizQuestion) => {
-          this.quizForm.addControl(quizQuestion.id.toString(), new FormControl(''));
+          this.quizForm.addControl(
+            quizQuestion.id.toString(),
+            new FormControl('', Validators.required),
+          );
         });
       },
       error: (err) => {
@@ -59,6 +62,8 @@ export class TriviaQuiz implements OnInit {
   }
 
   onSubmit() {
+    this.clearData();
+
     this.allQuestions.forEach((quizQuestion) => {
       this.playerResponses.push({
         questionId: quizQuestion.id,
@@ -69,22 +74,38 @@ export class TriviaQuiz implements OnInit {
     this.triviaQuizService.checkAnswers(this.playerResponses).subscribe({
       next: (data) => {
         this.playerResponseValidationResults.set(data);
+        this.computeScore();
+        this.showResults.set(true);
       },
       error: (err) => {
         console.error('Failed to retrieve answers validation', err);
       },
     });
-
-    this.showResults.set(true);
-  }
-
-  private getSelectedAnswerPerQuestion(quizQuestion: QuizQuestion): string {
-    return this.quizForm.get(quizQuestion.id.toString())?.value;
   }
 
   getValidationResult(quizQuestion: QuizQuestion): PlayerAnswerValidationResponse | undefined {
     return this.playerResponseValidationResults().find(
       (response) => response.questionId === quizQuestion.id,
     );
+  }
+
+  isDisabled(): boolean {
+    return this.quizForm.invalid;
+  }
+
+  private getSelectedAnswerPerQuestion(quizQuestion: QuizQuestion): string {
+    return this.quizForm.get(quizQuestion.id.toString())?.value;
+  }
+
+  private computeScore() {
+    this.score.set(
+      this.playerResponseValidationResults().filter((x) => x.isPlayerAnswerCorrect).length,
+    );
+  }
+
+  private clearData() {
+    this.score.set(0);
+    this.playerResponses = [];
+    this.playerResponseValidationResults.set([]);
   }
 }
